64e91c5e7ed01327d70bcca0118ea5bda082d26c
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-02-06 09:35:56 +0700

    Add SQL script to create and populate 'cities' table

---
+++ learn_postgresql.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cities (
+                        name VARCHAR(50),
+                        country VARCHAR(50),
+                        population INTEGER,
+                        area INTEGER
+);
+
+INSERT INTO cities (name, country, population, area)
+VALUES
+    ('Delhi', 'India', 28125000, 2240),
+    ('Shanghai', 'China', 22125000, 4015),
+    ('Sao Paulo', 'Brazil', 29835000, 3043);
