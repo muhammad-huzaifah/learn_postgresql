9e29389dd05ff021ef8be4176ec8e94cb324d151
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-02-17 20:26:11 +0700

    Add queries to retrieve data from 'cities' table

--- learn_postgresql.sql
+++ learn_postgresql.sql
@@ -9,4 +9,11 @@ INSERT INTO cities (name, country, population, area)
 VALUES
     ('Delhi', 'India', 28125000, 2240),
     ('Shanghai', 'China', 22125000, 4015),
-    ('Sao Paulo', 'Brazil', 29835000, 3043)
+    ('Sao Paulo', 'Brazil', 29835000, 3043);
+
+SELECT * FROM learn_postgresql.cities;
+
+SELECT name, country FROM learn_postgresql.cities;
+
+SELECT area, name, population  FROM learn_postgresql.cities;
+SELECT name, name, name  FROM learn_postgresql.cities;
