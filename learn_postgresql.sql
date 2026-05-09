2ba926e664575b66a41bf4387fbcaa626e4e450c
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-05-09 16:41:44 +0700

    Update queries to reference schema-qualified table 'learn_postgresql.cities' and add area-based filters

--- learn_postgresql.sql
+++ learn_postgresql.sql
@@ -22,25 +22,30 @@ SELECT name, population / area as population_per_area FROM learn_postgresql.citi
 SELECT name, population + area as population_per_area FROM learn_postgresql.cities;
 SELECT name, population * area as population_per_area FROM learn_postgresql.cities;
 
-SELECT name || country FROM cities;
+SELECT name || country FROM learn_postgresql.cities;
 
-SELECT name || ', ' || country FROM cities;
+SELECT name || ', ' || country FROM learn_postgresql.cities;
 
-SELECT name || ', ' || country AS location FROM cities;
+SELECT name || ', ' || country AS location FROM learn_postgresql.cities;
 
-SELECT CONCAT(name, country) AS location FROM cities;
+SELECT CONCAT(name, country) AS location FROM learn_postgresql.cities;
 
-SELECT CONCAT(name, ', ', country) AS location FROM cities;
+SELECT CONCAT(name, ', ', country) AS location FROM learn_postgresql.cities;
 
 SELECT
     CONCAT(UPPER(name), ', ', UPPER(country)) AS location
 FROM
-    cities;
+    learn_postgresql.cities;
 
 SELECT
     UPPER(CONCAT(name, ', ', country)) AS location
 FROM
-    cities;
+    learn_postgresql.cities;
 
-SELECT name, area FROM cities WHERE area > 4000;
+SELECT name, area FROM learn_postgresql.cities WHERE area > 4000;
 
+SELECT name, area FROM learn_postgresql.cities WHERE area = 8233;
+
+SELECT name, area FROM learn_postgresql.cities WHERE area != 8233;
+
+SELECT name, area FROM learn_postgresql.cities WHERE area <> 8233;
