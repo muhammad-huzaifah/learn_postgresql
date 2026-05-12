1eb72b2ae7638b55bda6a66a25404ed420b542fc
muhammad-huzaifah <ujebbawas@yahoo.com>
2026-05-12 07:31:07 +0700

    Add additional filtering examples using BETWEEN, NOT IN, and OR conditions in SQL

--- learn_postgresql.sql
+++ learn_postgresql.sql
@@ -48,4 +48,13 @@ SELECT name, area FROM learn_postgresql.cities WHERE area = 8233;
 
 SELECT name, area FROM learn_postgresql.cities WHERE area != 8233;
 
-SELECT name, area FROM learn_postgresql.cities WHERE area <> 8233;
+SELECT name, area FROM cities WHERE area <> 8233;
+
+SELECT name, area FROM cities WHERE area BETWEEN 2000 AND 5000;
+
+SELECT name, area FROM cities WHERE name NOT IN ('Delhi', 'Shanghai');
+
+SELECT name, area FROM cities WHERE area NOT IN (3043, 8223);
+
+SELECT name, area FROM cities WHERE area NOT IN (3043, 8223) OR name = 'Delhi' OR name = 'Tokyo';
+
